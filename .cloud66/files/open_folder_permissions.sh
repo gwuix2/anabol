5d58eb37469194c05c3a037ac43519906df0d9e6
gwuix2 <coolreed@gmail.com>
2013-08-14 11:29:10 +0200

    cloud66 setup

--- .cloud66/files/open_folder_permissions.sh
+++ .cloud66/files/open_folder_permissions.sh
@@ -3,4 +3,4 @@
 source /var/.cloud66_env
 #assign desired permissions
 sudo chmod 0777 -R $RAILS_STACK_PATH/public/uploads
-rackup faye.ru -s thin -E production
+rackup $RAILS_STACK_PATH/faye.ru -s thin -E production
