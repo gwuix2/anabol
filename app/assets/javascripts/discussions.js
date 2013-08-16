bc06941e7c2e12478656a7b2e5ede78683c381a2
gwuix2 <coolreed@gmail.com>
2013-08-16 13:16:03 +0200

    fix

--- app/assets/javascripts/discussions.js
+++ app/assets/javascripts/discussions.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://anabolizus.com:9292/faye');
+  var faye = new Faye.Client('http://anabolizmus.com:9292/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
