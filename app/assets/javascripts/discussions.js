1ca6cb82f5b830937d1d29b4fd5cbd5d924af2f8
gwuix2 <coolreed@gmail.com>
2013-08-17 09:51:58 +0200

    faye javascript

--- app/assets/javascripts/discussions.js
+++ app/assets/javascripts/discussions.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('localhost:9292/faye');
+  var faye = new Faye.Client('http://localhost:9292/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
