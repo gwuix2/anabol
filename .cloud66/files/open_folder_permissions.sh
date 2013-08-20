80041bdc8a6af83c2cbe94fdc09ea76598c68918
gwuix2 <coolreed@gmail.com>
2013-08-20 12:33:58 +0200

    assets path

--- .cloud66/files/open_folder_permissions.sh
+++ .cloud66/files/open_folder_permissions.sh
@@ -3,4 +3,3 @@
 source /var/.cloud66_env
 #assign desired permissions
 sudo chmod 0777 -R $RAILS_STACK_PATH/public/uploads
-sudo ln -nfs /var/deploy/anabol/web_head/shared/assets $RAILS_STACK_PATH/public/assets
